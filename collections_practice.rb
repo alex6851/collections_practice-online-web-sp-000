d2ea7aea0524782f75f6cd8c5ccece49fa8a29d9
Alexander Baker <alex.baker685@gmail.com>
2018-12-04 00:13:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
 
+def sort_array_asc(int_array)
+  int_array.sort { |a, b| a<=>b }
+end
+
+def sort_array_desc(int_array)
+  int_array.sort { |a, b| b<=>a }
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort { |a, b| a.length<=>b.length }
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_maker.each {|member| member[2].replace("$") }
+end
